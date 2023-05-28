0c3d43b394924644b7c91303009239fdfdd6dd3d
Molotovich <molotovich@gmail.com>
2023-05-28 16:34:08 -0600

    Agregadas las listas de frases

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,19 +1,19 @@
 package com.example.android.miwok;
 
 public class Word {
-     private String mEnglish;
-     private String mMiwok;
+    private final String mEnglish;
+    private final String mMiwok;
 
-     public Word(String english, String miwok){
-         mEnglish=english;
-         mMiwok=miwok;
-     }
+    public Word(String english, String miwok) {
+        mEnglish = english;
+        mMiwok = miwok;
+    }
 
-     public String getEnglish(){
-         return mEnglish;
-     }
+    public String getEnglish() {
+        return mEnglish;
+    }
 
-     public String getMiwok(){
-         return mMiwok;
-     }
+    public String getMiwok() {
+        return mMiwok;
+    }
 }
